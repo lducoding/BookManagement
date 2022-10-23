812ad3e390136081e3359b3b97911ed22c0797e0
lducoding <lducoding@gmail.com>
2022-10-23 23:00:06 +0900

    책의 바코드 생성 고민 중

---
+++ src/main/java/com/manage/book/controller/BookController.java
@@ -0,0 +1,12 @@
+package com.manage.book.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+
+    // 책의 아이디 발급을 어떻게 해야 할까?
+    // 바코드 발급 api? 를 만든다?
+
+}
--- src/main/java/com/manage/book/entity/Book.java
+++ src/main/java/com/manage/book/entity/Book.java
@@ -1,5 +1,14 @@
 package com.manage.book.entity;
 
+import javax.persistence.*;
+
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="book_id")
+    private Long bookId;
+
 
 }
