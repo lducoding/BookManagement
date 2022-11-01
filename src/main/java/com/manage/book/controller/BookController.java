4a9d38a8c0ab4ffb3da5f922c3d1986563decb9d
lducoding <lducoding@gmail.com>
2022-11-01 16:13:07 +0900

    책임 협력 역할에 대해 생각 중

--- src/main/java/com/manage/book/controller/BookController.java
+++ src/main/java/com/manage/book/controller/BookController.java
@@ -12,9 +12,14 @@ public class BookController {
 
     // 책의 아이디 발급을 어떻게 해야 할까?
     // 바코드 발급 api? 를 만든다?
+    // 신규 도서를 등록한다.
     @PostMapping("")
     public ResponseEntity<?> registerBook() {
         HttpHeaders httpHeaders = new HttpHeaders();
+        // 책의 정보를 등록하라 - 메시지
+
+
+
         return new ResponseEntity<Long>(, httpHeaders, HttpStatus.OK);
     }
 }
